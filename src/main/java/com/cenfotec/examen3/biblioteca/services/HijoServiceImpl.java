92ce2bb04ba18440fcf68c137c2e977194d88551
FSANDOVALC <fsandovalc@ucenfotec.ac.cr>
2022-05-03 21:49:16 -0600

    Servicio de Hijos

--- src/main/java/com/cenfotec/examen3/biblioteca/services/HijoServiceImpl.java
+++ src/main/java/com/cenfotec/examen3/biblioteca/services/HijoServiceImpl.java
@@ -16,26 +16,42 @@ public class HijoServiceImpl implements HijoService{
 
     @Override
     public List<Hijo> getAll() {
-        return null;
+        return hijoRepository.findAll();
     }
 
     @Override
     public Optional<Hijo> findById(Long id) {
-        return Optional.empty();
+        return hijoRepository.findById(id).map(record -> Optional.of(record)).orElse(Optional.empty());
     }
 
     @Override
     public Optional<Hijo> save(Hijo hijo) {
-        return Optional.empty();
+        return Optional.of(hijoRepository.save(hijo));
     }
 
     @Override
     public Optional<Hijo> update(Hijo hijo) {
+        Optional<Hijo> record = hijoRepository.findById(hijo.getId());
+        if (record.isPresent()) {
+            Hijo data = record.get();
+            data.setNombre(hijo.getNombre());
+            data.setPrimerApellido(hijo.getPrimerApellido());
+            data.setSegundoApellido(hijo.getSegundoApellido());
+            data.setPlanGuarderia(hijo.isPlanGuarderia());
+            data.setPlanLectura(hijo.isPlanLectura());
+            data.setAlergias(hijo.getAlergias());
+            return Optional.of(hijoRepository.save(data));
+        }
         return Optional.empty();
     }
 
     @Override
     public boolean delete(Long id) {
+        Optional<Hijo> result = hijoRepository.findById(id);
+        if (result.isPresent()){
+            hijoRepository.deleteById(id);
+            return true;
+        }
         return false;
     }
 }
