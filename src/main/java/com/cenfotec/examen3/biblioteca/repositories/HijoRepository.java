eaca7b001a743d618c515ffef6be3e48d83b9758
FSANDOVALC <fsandovalc@ucenfotec.ac.cr>
2022-05-03 23:54:37 -0600

    Codigo funcional REST

--- src/main/java/com/cenfotec/examen3/biblioteca/repositories/HijoRepository.java
+++ src/main/java/com/cenfotec/examen3/biblioteca/repositories/HijoRepository.java
@@ -2,6 +2,8 @@ package com.cenfotec.examen3.biblioteca.repositories;
 
 import com.cenfotec.examen3.biblioteca.domains.Hijo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HijoRepository extends JpaRepository<Hijo, Long> {
 }
--- src/main/java/com/cenfotec/examen3/biblioteca/repositories/PadreFamiliaRepository.java
+++ src/main/java/com/cenfotec/examen3/biblioteca/repositories/PadreFamiliaRepository.java
@@ -2,6 +2,8 @@ package com.cenfotec.examen3.biblioteca.repositories;
 
 import com.cenfotec.examen3.biblioteca.domains.PadreFamilia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PadreFamiliaRepository extends JpaRepository<PadreFamilia, Long> {
 }
